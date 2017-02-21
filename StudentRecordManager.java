684422945b918acbe7314bead623172d70972b5e
Ivy Chang <ivycwq@icloud.com>
2017-02-21 13:36:30 -0800

    implementing student record manager class

--- StudentRecordManager.java
+++ StudentRecordManager.java
@@ -1,3 +1,10 @@
-public class StudentRecordManager {
+import java.util.ArrayList;
+
+public class StudentRecordManager implements Iterable<Student> {
+  private static ArrayList<Student> studentList;
+
+  public MyIterator<Student> iterator() {
+    return new MyIterator<Student>(studentList);
+  } 
 
 }
